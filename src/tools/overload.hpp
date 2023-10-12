e26584cef53a3adc4ce2128fc8c8aab2bd716c58
Wim Haeck <whaeck@gmail.com>
2023-10-11 22:02:47 -0600

    Adding the overload struct to tools

---
+++ src/tools/overload.hpp
@@ -0,0 +1,32 @@
+#ifndef NJOY_TOOLS_OVERLOAD
+#define NJOY_TOOLS_OVERLOAD
+
+// system includes
+
+// other includes
+
+namespace njoy {
+namespace tools {
+
+  /**
+   *  @brief Overload struct for collecting lambdas for std::visit
+   *
+   *  For example:
+   *
+   *    auto value = std::visit(
+   *        tools::overload{ [] ( const Foo& ) -> std::string
+   *                            { return "Foo"; },
+   *                         [] ( const Bar& )
+   *                            { return "Bar"; } },
+   *        variant );
+   */
+  template < class... Types > struct overload : Types... {
+
+    using Types::operator()...;
+  };
+  template < class... Types > overload( Types... ) -> overload< Types... >;
+
+} // tools namespace
+} // njoy namespace
+
+#endif
