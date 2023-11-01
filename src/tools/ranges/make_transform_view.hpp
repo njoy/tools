e7a1e0b42f76b32092f65e1734ab0ddc00286435
Wim Haeck <whaeck@gmail.com>
2023-11-01 15:53:15 -0600

    updating make_transform_view

--- src/tools/ranges/make_transform_view.hpp
+++ src/tools/ranges/make_transform_view.hpp
@@ -4,31 +4,14 @@
 // system includes
 
 // other includes
-#include "tools/ranges/IteratorView.hpp"
-#include "tools/ranges/TransformIterator.hpp"
+#include "tools/ranges/TransformView.hpp"
 
 namespace njoy {
 namespace tools {
 namespace ranges {
 
 /**
- *  @brief Make an IteratorView based on two iterators and a transformation
- *
- *  @param[in] begin             the iterator to the beginning of the view
- *  @param[in] end               the iterator to the end of the view
- *  @param[in] transformation    the transformation to be applied
- */
-template < typename Iterator, typename Transformation >
-constexpr auto make_transform_view( Iterator begin, Iterator end,
-                                    Transformation transformation ) {
-
-  using Iter = TransformIterator< Iterator, Transformation >;
-  return IteratorView< Iter >{ Iter( std::move( begin ), transformation ),
-                               Iter( std::move( end ), transformation ) };
-}
-
-/**
- *  @brief Make an IteratorView based on a container and a transformation
+ *  @brief Make an TransformView based on a container and a transformation
  *
  *  @param[in] container         the container
  *  @param[in] transformation    the transformation to be applied
@@ -37,8 +20,7 @@ template < typename Container, typename Transformation >
 constexpr auto make_transform_view( Container&& container,
                                     Transformation transformation ) {
 
-  return make_transform_view( container.begin(), container.end(),
-                              std::move( transformation ) );
+  return TransformView( container, std::move( transformation ) );
 }
 
 } // ranges namespace
