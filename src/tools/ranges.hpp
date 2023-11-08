1a6aa3f32689351cc5ba652f1e9d06eb77732df5
Wim Haeck <whaeck@gmail.com>
2023-11-08 12:35:12 -0700

    Updating include

--- src/tools/ranges.hpp
+++ src/tools/ranges.hpp
@@ -1,5 +1,6 @@
 #include "tools/ranges/IteratorView.hpp"
-#include "tools/ranges/TransformIterator.hpp"
+#include "tools/ranges/TransformView.hpp"
+#include "tools/ranges/AnyView.hpp"
 
 #include "tools/ranges/make_view.hpp"
 #include "tools/ranges/make_transform_view.hpp"
