3fb63e2052f71bad06a4fae9ce5dc2797c5ef010
Wim Haeck <whaeck@gmail.com>
2024-02-14 15:41:26 -0700

    Adding common_reference with tests

--- src/tools/std20/type_traits.hpp
+++ src/tools/std20/type_traits.hpp
@@ -10,6 +10,8 @@ namespace njoy {
 namespace tools {
 namespace std20 {
 
+// remove_cvref
+
 template < typename T >
 struct remove_cvref {
   using type = std::remove_cv_t< std::remove_reference_t< T > >;
@@ -18,6 +20,8 @@ struct remove_cvref {
 template < typename T >
 using remove_cvref_t = typename remove_cvref< T >::type;
 
+// type_identity
+
 template < typename T >
 struct type_identity {
   using type = T;
@@ -25,6 +29,47 @@ struct type_identity {
 
 template < typename T >
 using type_identity_t = typename type_identity< T >::type;
+
+// basic_common_reference
+
+template < typename T, typename U, template < typename > typename TQual,
+           template < typename > typename UQual>
+struct basic_common_reference {};
+
+// common_reference
+
+template < typename... >
+struct common_reference;
+
+template < typename... Ts >
+using common_reference_t = typename common_reference< Ts... >::type;
+
+#include "tools/std20/type_traits/details/common_reference.hpp"
+
+template <>
+struct common_reference<> {};
+
+template < typename T >
+struct common_reference< T > {
+
+    using type = T;
+};
+
+template < typename T1, typename T2 >
+struct common_reference< T1, T2 > : detail::binary_common_ref<T1, T2> {};
+
+template < typename T1, typename T2, typename... Rest >
+struct common_reference< T1, T2, Rest... >
+  : detail::multiple_common_reference< void, T1, T2, Rest... > {};
+
+// is_bounded_array
+// is_unbounded_array
+// is_nothrow_convertible
+// is_layout_compatible
+// is_pointer_interconvertible_base_of
+// is_pointer_interconvertible_with_class
+// is_corresponding_member
+// is_constant_evaluated
 
 } // std20 namespace
 } // tools namespace
