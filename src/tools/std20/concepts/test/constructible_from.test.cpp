831c2ccf640fae725bbfce7bea11d64b5e1524b9
Wim Haeck <whaeck@gmail.com>
2024-02-26 15:38:07 -0700

    More tests

---
+++ src/tools/std20/concepts/test/constructible_from.test.cpp
@@ -0,0 +1,93 @@
+// include Catch2
+#include <catch2/catch_test_macros.hpp>
+
+// what we are testing
+#include "tools/std20/concepts.hpp"
+
+// other includes
+#include <string>
+
+// convenience typedefs
+//using namespace njoy::tools;
+namespace std20 = nano::ranges;
+
+// test code
+struct Foo {
+
+  ~Foo() noexcept {};
+};
+
+struct NonDefaultConstructableFoo {
+
+  NonDefaultConstructableFoo() = delete;
+  ~NonDefaultConstructableFoo() noexcept {};
+};
+
+struct Bar {
+
+  Bar() = default;
+  explicit Bar( int );
+  ~Bar() = default;
+};
+
+SCENARIO( "constructible_from" ) {
+
+  CHECK( std20::constructible_from< Foo > );
+  CHECK( std20::constructible_from< Bar > );
+  CHECK( std20::constructible_from< std::string > );
+  CHECK( std20::constructible_from< Bar, int > );
+
+  CHECK( ! std20::constructible_from< NonDefaultConstructableFoo > );
+
+  CHECK( std20::constructible_from< int > );
+  CHECK( std20::constructible_from< const int > );
+  CHECK( ! std20::constructible_from< const int& > );
+  CHECK( ! std20::constructible_from< int() > );
+  CHECK( ! std20::constructible_from< int (&)() > );
+  CHECK( ! std20::constructible_from< int[] > );
+  CHECK( std20::constructible_from< int[2] > );
+  CHECK( std20::constructible_from< int const (&)[2], int (&)[2] > );
+  CHECK( ! std20::constructible_from< int, int (&)[2] >);
+
+  CHECK( std20::constructible_from< int, int > );
+  CHECK( std20::constructible_from< int, int& > );
+  CHECK( std20::constructible_from< int, int&& > );
+  CHECK( std20::constructible_from< int, const int > );
+  CHECK( std20::constructible_from< int, const int& > );
+  CHECK( std20::constructible_from< int, const int&& > );
+
+  CHECK( std20::constructible_from< Foo, Foo > );
+  CHECK( std20::constructible_from< Foo, Foo& > );
+  CHECK( std20::constructible_from< Foo, Foo&& > );
+  CHECK( std20::constructible_from< Foo, const Foo > );
+  CHECK( std20::constructible_from< Foo, const Foo& > );
+  CHECK( std20::constructible_from< Foo, const Foo&&> );
+
+  CHECK( ! std20::constructible_from< int&, int > );
+  CHECK( std20::constructible_from< int&, int& > );
+  CHECK( ! std20::constructible_from< int&, int&& > );
+  CHECK( ! std20::constructible_from< int&, const int > );
+  CHECK( ! std20::constructible_from< int&, const int& > );
+  CHECK( ! std20::constructible_from< int&, const int&& > );
+
+  CHECK( std20::constructible_from< const int&, int > );
+  CHECK( std20::constructible_from< const int&, int& > );
+  CHECK( std20::constructible_from< const int&, int&& > );
+  CHECK( std20::constructible_from< const int&, const int > );
+  CHECK( std20::constructible_from< const int&, const int& > );
+  CHECK( std20::constructible_from< const int&, const int&& > );
+
+  CHECK( std20::constructible_from< int&&, int > );
+  CHECK( ! std20::constructible_from< int&&, int& > );
+  CHECK( std20::constructible_from< int&&, int&& > );
+  CHECK( ! std20::constructible_from< int&&, const int > );
+  CHECK( ! std20::constructible_from< int&&, const int& > );
+  CHECK( ! std20::constructible_from< int&&, const int&& > );
+
+  CHECK( std20::constructible_from< const int&&, int > );
+  CHECK(! std20::constructible_from< const int&&, int& > );
+  CHECK( std20::constructible_from< const int&&, int&& > );
+  CHECK( std20::constructible_from< const int&&, const int > );
+  CHECK(! std20::constructible_from< const int&&, const int& > );
+  CHECK( std20::constructible_from< const int&&, const int&& > );
+} // SCENARIO
---
+++ src/tools/std20/concepts/test/destructible.test.cpp
@@ -0,0 +1,57 @@
+// include Catch2
+#include <catch2/catch_test_macros.hpp>
+
+// what we are testing
+#include "tools/std20/concepts.hpp"
+
+// other includes
+#include <string>
+
+// convenience typedefs
+//using namespace njoy::tools;
+namespace std20 = nano::ranges;
+
+// test code
+struct Foo {
+
+  ~Foo() noexcept {};
+};
+
+struct IndestructibleFoo {
+
+  ~IndestructibleFoo() = delete;
+};
+
+struct ThrowingDestructorFoo {
+
+  ~ThrowingDestructorFoo() noexcept(false);
+};
+
+struct Bar {
+
+  ~Bar() = default;
+};
+
+SCENARIO( "destructible" ) {
+
+  CHECK( std20::destructible< Foo > );
+  CHECK( std20::destructible< Bar > );
+  CHECK( std20::destructible< std::string > );
+
+  CHECK( ! std20::destructible< IndestructibleFoo > );
+  CHECK( ! std20::destructible< ThrowingDestructorFoo > );
+
+  CHECK( std20::destructible< int > );
+  CHECK( std20::destructible< const int > );
+
+  CHECK( std20::destructible< int& > );
+  CHECK( std20::destructible< void (*)() > );
+  CHECK( std20::destructible< void (&)() > );
+  CHECK( std20::destructible< int[2] > );
+  CHECK( std20::destructible< int (*)[2] > );
+  CHECK( std20::destructible< int (&)[2] > );
+
+  CHECK( ! std20::destructible< void > );
+  CHECK( ! std20::destructible< void() > );
+  CHECK( ! std20::destructible< int[] > );
+} // SCENARIO
