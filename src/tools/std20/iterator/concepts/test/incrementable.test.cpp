e520986dcb708a462e843f7e6c38d18a5f010c0c
Wim Haeck <whaeck@gmail.com>
2024-02-27 14:38:11 -0700

    a few more tests for concepts

---
+++ src/tools/std20/iterator/concepts/test/incrementable.test.cpp
@@ -0,0 +1,26 @@
+// include Catch2
+#include <catch2/catch_test_macros.hpp>
+
+// what we are testing
+#include "tools/std20/iterator.hpp"
+
+// other includes
+
+// convenience typedefs
+//using namespace njoy::tools;
+namespace std20 = nano::ranges;
+
+SCENARIO( "incrementable and weakly_incrementable" ) {
+
+  CHECK( std20::weakly_incrementable< int > );
+  CHECK( std20::weakly_incrementable< unsigned int > );
+  CHECK( ! std20::weakly_incrementable< void > );
+  CHECK( std20::weakly_incrementable< int* > );
+  CHECK( std20::weakly_incrementable< const int* > );
+
+  CHECK( std20::incrementable< int > );
+  CHECK( std20::incrementable< unsigned int > );
+  CHECK( ! std20::incrementable< void > );
+  CHECK( std20::incrementable< int* > );
+  CHECK( std20::incrementable< const int* > );
+} // SCENARIO
