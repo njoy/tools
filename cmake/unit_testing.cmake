4e24229f52907f81d1fb61e6bfcadc0268207e5a
Wim Haeck <whaeck@gmail.com>
2024-02-27 12:48:09 -0700

    Starting iterator concept tests

--- cmake/unit_testing.cmake
+++ cmake/unit_testing.cmake
@@ -29,4 +29,5 @@ endfunction()
 add_subdirectory( src/tools/Log/test )
 
 add_subdirectory( src/tools/std20/concepts/test )
+add_subdirectory( src/tools/std20/iterator/concepts/test )
 add_subdirectory( src/tools/std20/type_traits/test )
