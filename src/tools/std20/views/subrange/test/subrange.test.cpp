bd437ddd0342335d100a375fb3f4cec16219d7a9
Wim Haeck <whaeck@gmail.com>
2024-02-29 12:26:27 -0700

    starting with views

---
+++ src/tools/std20/views/subrange/test/subrange.test.cpp
@@ -0,0 +1,15 @@
+// include Catch2
+#include <catch2/catch_test_macros.hpp>
+
+// what we are testing
+#include "tools/std20/views.hpp"
+
+// other includes
+
+// convenience typedefs
+//using namespace njoy::tools;
+namespace std20 = nano::ranges;
+
+SCENARIO( "subrange" ) {
+
+} // SCENARIO
