0af9594c3131d5e2d1032727d210f8a9cbf1925c
Wim Haeck <whaeck@gmail.com>
2024-02-29 06:32:23 -0700

    more common_iterator

--- src/tools/std20/iterator/common_iterator/test/common_iterator.test.cpp
+++ src/tools/std20/iterator/common_iterator/test/common_iterator.test.cpp
@@ -30,9 +30,11 @@ SCENARIO( "common_iterator" ) {
   CHECK( std::is_constructible_v< Iter, const int* > );
 
   std::vector< Foo > values = { {1}, {2} };
-  std20::common_iterator< std::vector< Foo >::iterator,
-                          std::vector< Foo >::const_iterator > begin = values.begin(),
-                                                               end = values.cend();
+  using CommonVector = std20::common_iterator< std::vector< Foo >::iterator,
+                                               std::vector< Foo >::const_iterator >;
+  CommonVector begin = values.begin();
+  CommonVector end = values.cend();
+
   CHECK( *begin == 1 );
   CHECK( begin->n == 1 );
   ++begin;
@@ -40,4 +42,31 @@ SCENARIO( "common_iterator" ) {
   CHECK( begin->n == 2 );
   ++begin;
   CHECK( begin == end );
+
+  int array[2] = { 1, 2 };
+  std20::common_iterator< int*, const int* > i = std::begin( array );
+  std20::common_iterator< int*, const int* > arrayend = std::cend( array );
+
+  CHECK( i != arrayend );
+  CHECK( (arrayend - i) == 2 );
+  CHECK( (i - arrayend) == -2 );
+  auto j = i;
+  CHECK( j == i );
+  CHECK( (j - i) == 0 );
+  j = arrayend;
+  CHECK( j != i );
+  CHECK( j == arrayend );
+  j = std::next(i);
+  CHECK( j != i );
+  CHECK( j != arrayend );
+  CHECK( (arrayend - j) == 1 );
+  CHECK( (j - i) == 1 );
+  CHECK( (i - j) == -1 );
+  ++j;
+  CHECK( j == arrayend );
+  CHECK( (arrayend - j) == 0 );
+  j = i;
+  CHECK( j == i );
+  CHECK( (j - arrayend) == -2 );
+  CHECK( (j - i) == 0 );
 } // SCENARIO
