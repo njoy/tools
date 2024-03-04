ba816022e70e766a412a646626740053a0b2e722
Wim Haeck <whaeck@gmail.com>
2024-03-04 15:11:08 -0700

    Moving some tests, adding test for readable_traits

---
+++ src/tools/std20/iterator/test/readable_traits.test.cpp
@@ -0,0 +1,57 @@
+// include Catch2
+#include <catch2/catch_test_macros.hpp>
+
+// what we are testing
+#include "tools/std20/iterator.hpp"
+
+// other includes
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <set>
+#include <string>
+#include <string_view>
+#include <vector>
+
+// convenience typedefs
+//using namespace njoy::tools;
+namespace std20 = nano::ranges;
+
+SCENARIO( "readable_traits" ) {
+
+  CHECK( std20::same_as< std20::readable_traits< int* >::value_type, int > );
+  CHECK( std20::same_as< std20::readable_traits< const int* >::value_type, int > );
+  CHECK( std20::same_as< std20::readable_traits< int[] >::value_type, int > );
+  CHECK( std20::same_as< std20::readable_traits< const int[] >::value_type, int > );
+
+  CHECK( std20::same_as< std20::readable_traits< std::array< int, 1 >::iterator >::value_type, int > );
+  CHECK( std20::same_as< std20::readable_traits< std::array< const int, 1 >::iterator >::value_type, int > );
+
+  CHECK( std20::same_as< std20::readable_traits< std::deque< int >::iterator >::value_type, std::deque< int >::iterator::value_type > );
+  CHECK( std20::same_as< std20::readable_traits< std::deque< const int >::iterator >::value_type, std::deque< const int >::iterator::value_type > );
+
+  CHECK( std20::same_as< std20::readable_traits< std::forward_list< int >::iterator >::value_type, std::forward_list< int >::iterator::value_type > );
+  CHECK( std20::same_as< std20::readable_traits< std::forward_list< const int >::iterator >::value_type, std::forward_list< const int >::iterator::value_type > );
+
+  CHECK( std20::same_as< std20::readable_traits< std::list< int >::iterator >::value_type, std::list< int >::iterator::value_type > );
+  CHECK( std20::same_as< std20::readable_traits< std::list< const int >::iterator >::value_type, std::list< const int >::iterator::value_type > );
+
+  CHECK( std20::same_as< std20::readable_traits< std::vector< int >::iterator >::value_type, std::vector< int >::iterator::value_type > );
+  CHECK( std20::same_as< std20::readable_traits< std::vector< const int >::iterator >::value_type, std::vector< const int >::iterator::value_type > );
+
+  CHECK( std20::same_as< std20::readable_traits< const std::array< int, 1 >::iterator >::value_type, int > );
+  CHECK( std20::same_as< std20::readable_traits< const std::array< const int, 1 >::iterator >::value_type, int > );
+
+  CHECK( std20::same_as< std20::readable_traits< const std::deque< int >::iterator >::value_type, std::deque< int >::iterator::value_type > );
+  CHECK( std20::same_as< std20::readable_traits< const std::deque< const int >::iterator >::value_type, std::deque< const int >::iterator::value_type > );
+
+  CHECK( std20::same_as< std20::readable_traits< const std::forward_list< int >::iterator >::value_type, std::forward_list< int >::iterator::value_type > );
+  CHECK( std20::same_as< std20::readable_traits< const std::forward_list< const int >::iterator >::value_type, std::forward_list< const int >::iterator::value_type > );
+
+  CHECK( std20::same_as< std20::readable_traits< const std::list< int >::iterator >::value_type, std::list< int >::iterator::value_type > );
+  CHECK( std20::same_as< std20::readable_traits< const std::list< const int >::iterator >::value_type, std::list< const int >::iterator::value_type > );
+
+  CHECK( std20::same_as< std20::readable_traits< const std::vector< int >::iterator >::value_type, std::vector< int >::iterator::value_type > );
+  CHECK( std20::same_as< std20::readable_traits< const std::vector< const int >::iterator >::value_type, std::vector< const int >::iterator::value_type > );
+} // SCENARIO
