9f975939dd0c78a79913646a3f938a763e70407e
Wim Haeck <whaeck@gmail.com>
2024-05-02 11:34:22 -0600

    adding nonpropegating_box

---
+++ src/tools/std23/details/views/nonpropegating_box.hpp
@@ -0,0 +1,96 @@
+#ifndef NJOY_TOOLS_STD23_RANGES_VIEWS_CHUNK_BY
+#define NJOY_TOOLS_STD23_RANGES_VIEWS_CHUNK_BY
+
+#include <optional>
+
+namespace njoy {
+namespace tools {
+namespace std23 {
+inline namespace ranges {
+
+namespace detail {
+
+/**
+ *  @brief A helper type containing a value that gets reset upon move or copy
+ *    
+ *  Similar to the semiregular_box, the nonpropegating_box is a helper type for
+ *  implementing views. It stores an optional containing a value that gets
+ *  reset upon move or copy.
+ *    
+ */
+template < typename T >
+struct nonpropegating_box : std::optional<T> {
+
+  static_assert( std::is_object_v< T > );
+
+private:
+
+  struct from_tag {};
+  struct forward_tag {};
+
+/*  struct wrapper {
+
+    T t_;
+
+    template < typename... Args >
+    constexpr explicit wrapper( forward_tag, Args&&... args ) : 
+        t_( std::forward< Args >( args )... ) {}
+
+    template < typename F >
+    constexpr explicit wrapper( from_tag, const F& f ) : 
+        t_( f() ) {}
+  };*/
+
+//  std::optional< wrapper > value_ = std::nullopt;
+  std::optional< T > value_ = std::nullopt;
+
+public:
+
+  nonpropegating_box() = default;
+
+  constexpr nonpropegating_box( const nonpropegating_box& ) : value_( std::nullopt ) {}
+
+  constexpr nonpropegating_box( nonpropegating_box&& other ) : 
+      value_( std::nullopt ) {
+
+    other.value_.reset();
+  }
+
+  constexpr nonpropegating_box& operator=( const nonpropegating_box& other ) {
+
+    if ( this != std::addressof( other ) ) {
+
+      this->value_.reset();
+    }
+    return *this;
+  }
+
+  constexpr nonpropegating_box& operator=( nonpropegating_box&& other ) {
+
+    this->value_.reset();
+    other.value_.reset();
+    return *this;
+  }
+
+  constexpr T& operator*() { return this->value_.value(); }
+
+  constexpr const T& operator*() const { return this->value_.value(); }
+
+  constexpr bool has_value() const { return this->value_.has_value(); }
+
+  template < typename... Args >
+  constexpr T& emplace( Args&&... args ) {
+
+    return this->value_.emplace( std::forward< Args >( args )... );
+  }
+};
+
+} // namespace detail
+
+} // namespace ranges
+} // namespace std23
+} // namespace tools
+} // namespace njoy
+
+#endif
+
---
+++ src/tools/std23/details/views/test/nonpropegating_box.test.cpp
@@ -0,0 +1,87 @@
+// include Catch2
+#include <catch2/catch_test_macros.hpp>
+
+// what we are testing
+#include "tools/std23/details/views/nonpropegating_box.hpp"
+
+// other includes
+#include <vector>
+
+// convenience typedefs
+using namespace njoy::tools;
+
+SCENARIO( "nonpropegating_box" ) {
+
+  std::vector< double > values = { 1., 2., 3., 4. };
+  using Iterator = std::vector< double >::iterator;
+
+  THEN( "the default constructor makes an empty box" ) {
+
+    // default constructor: no content
+    std23::ranges::detail::nonpropegating_box< Iterator > box;
+    CHECK( false == box.has_value() );
+  } // THEN
+
+  THEN( "emplace sets the content in the box" ) {
+
+    std23::ranges::detail::nonpropegating_box< Iterator > box;
+    CHECK( false == box.has_value() );
+
+    // emplace
+    auto value = box.emplace( values.begin() );
+    CHECK( true == box.has_value() );
+    CHECK( values.begin() == value );
+    CHECK( values.begin() == *box );
+  } // THEN
+
+  THEN( "copy constructor" ) {
+
+    std23::ranges::detail::nonpropegating_box< Iterator > box;
+    box.emplace( values.begin() );
+
+    // copy construction: other has no content, box unchanged
+    std23::ranges::detail::nonpropegating_box< Iterator > other( box );
+    CHECK( false == other.has_value() );
+    CHECK( true == box.has_value() );
+  } // THEN
+
+  THEN( "move constructor" ) {
+
+    std23::ranges::detail::nonpropegating_box< Iterator > box;
+    box.emplace( values.begin() );
+
+    // move construction: other has no content, box reset
+    std23::ranges::detail::nonpropegating_box< Iterator > other( std::move( box ) );
+    CHECK( false == other.has_value() );
+    CHECK( false == box.has_value() );
+  } // THEN
+
+  THEN( "copy assignment" ) {
+
+    std23::ranges::detail::nonpropegating_box< Iterator > box, other;
+    box.emplace( values.begin() );
+    other.emplace( values.end() );
+    CHECK( true == box.has_value() );
+    CHECK( true == other.has_value() );
+
+    // copy assignment: reset the content of other, box unchanged
+    other = box;
+    CHECK( true == box.has_value() );
+    CHECK( false == other.has_value() );
+  } // THEN
+
+  THEN( "move assignment" ) {
+
+    std23::ranges::detail::nonpropegating_box< Iterator > box, other;
+    box.emplace( values.begin() );
+    other.emplace( values.end() );
+    CHECK( true == box.has_value() );
+    CHECK( true == other.has_value() );
+
+    // move assignment: reset the content of other and box
+    other = std::move( box );
+    CHECK( false == box.has_value() );
+    CHECK( false == other.has_value() );
+  } // THEN
+} // SCENARIO
+
