821d48eb6f07773ef4ed362065799b9fdc8fa68f
Wim Haeck <whaeck@gmail.com>
2024-05-03 11:00:56 -0600

    finishing chunk_by

--- src/tools/std23/views/chunk_by.hpp
+++ src/tools/std23/views/chunk_by.hpp
@@ -22,7 +22,9 @@ struct chunk_by_view : std20::ranges::view_interface< chunk_by_view< R, Pred > >
   static_assert( std20::ranges::view< R > );
   static_assert( std20::ranges::input_range< R > );
   static_assert( std::is_object_v< Pred > );
-  static_assert( std20::ranges::indirect_binary_predicate< const Pred, std20::ranges::iterator_t< R >, std20::ranges::iterator_t< R > > );
+  static_assert( std20::ranges::indirect_binary_predicate<
+                     const Pred, std20::ranges::iterator_t< R >,
+                     std20::ranges::iterator_t< R > > );
 
 private:
 
@@ -40,7 +42,7 @@ private:
     std20::ranges::iterator_t< R > current_ = std20::ranges::iterator_t< R >();
     std20::ranges::iterator_t< R > next_ = std20::ranges::iterator_t< R >();
 
-    constexpr iterator( chunk_by_view& parent, std20::ranges::iterator_t< R > current, 
+    constexpr iterator( chunk_by_view& parent, std20::ranges::iterator_t< R > current,
                         std20::ranges::iterator_t< R > next ) :
         parent_( std::addressof( parent ) ), current_( current ), next_( next ) {}
 
@@ -48,45 +50,44 @@ private:
 
     using value_type        = std20::ranges::subrange< std20::ranges::iterator_t< R > >;
     using difference_type   = std20::ranges::range_difference_t< R >;
-    using iterator_category = std::input_iterator_tag;
-    using iterator_concept  = std::conditional_t< std20::ranges::bidirectional_range< R >, 
-                                                  std20::bidirectional_iterator_tag, 
+    using iterator_category = std::conditional_t< std20::ranges::bidirectional_range< R >,
+                                                  std20::bidirectional_iterator_tag,
                                                   std20::forward_iterator_tag >;
 
     iterator() = default;
 
     constexpr value_type operator*() const { return { this->current_, this->next_ }; }
 
-    constexpr iterator& operator++() const { 
+    constexpr iterator& operator++() {
 
       this->current_ = this->next_;
       this->next_ = this->parent_->find_next( this->current_ );
-      return *this; 
+      return *this;
     }
 
-    constexpr iterator& operator++(int) const { 
+    constexpr iterator operator++(int) {
 
       auto temp = *this;
       ++*this;
-      return temp; 
+      return temp;
     }
 
     template < typename RR = R,
                typename = std::enable_if_t< std20::ranges::bidirectional_range< RR > > >
-    constexpr iterator& operator--() const { 
+    constexpr iterator& operator--() {
 
       this->next_ = this->current_;
       this->current_ = this->parent_->find_prev( this->next_ );
-      return *this; 
+      return *this;
     }
 
     template < typename RR = R,
                typename = std::enable_if_t< std20::ranges::bidirectional_range< RR > > >
-    constexpr iterator& operator--(int) const { 
+    constexpr iterator operator--(int) {
 
       auto temp = *this;
       --*this;
-      return temp; 
+      return temp;
     }
 
     constexpr bool operator==( const iterator& right ) const {
@@ -102,43 +103,45 @@ private:
 
   constexpr std20::ranges::iterator_t< R > find_next( std20::ranges::iterator_t< R > current ) {
 
-    //static_assert( this->pred_.has_value() );
+    assert( this->pred_.has_value() );
 
-    auto not_pred = [&pred = this->pred_.value()] ( auto&& x, auto&& y ) {
+    auto not_pred = [&pred = this->pred_.value()] ( auto&& x, auto&& y ) -> bool {
 
-      return !std20::ranges::invoke( pred, 
-                                     std::forward< decltype(x) >( x ), 
+      return !std20::ranges::invoke( pred,
+                                     std::forward< decltype(x) >( x ),
                                      std::forward< decltype(y) >( y ) );
     };
 
     auto end = std20::ranges::end( this->base_ );
-    return std20::ranges::next( std20::ranges::adjacent_find( current, end, not_pred ), end ); 
+    return std20::ranges::next( std20::ranges::adjacent_find( current, end, not_pred ), 1, end );
   }
 
   template < typename RR = R,
              typename = std::enable_if_t< std20::ranges::bidirectional_range< RR > > >
   constexpr std20::ranges::iterator_t< R > find_prev( std20::ranges::iterator_t< R > current ) {
 
-    static_assert( current != std20::ranges::begin( this->base_ ) );
-    static_assert( this->pred_.has_value() );
+    assert( current != std20::ranges::begin( this->base_ ) );
+    assert( this->pred_.has_value() );
 
-    auto not_pred = [&pred = this->pred_.value()] ( auto&& x, auto&& y ) {
+    auto not_pred = [&pred = this->pred_.value()] ( auto&& x, auto&& y ) -> bool {
 
-      return !std20::ranges::invoke( pred, 
-                                     std::forward< decltype(y) >( y ), 
+      return !std20::ranges::invoke( pred,
+                                     std::forward< decltype(y) >( y ),
                                      std::forward< decltype(x) >( x ) );
     };
-    
+
     auto begin = std20::ranges::begin( this->base_ );
     std20::ranges::reverse_view reverse( std20::ranges::subrange( begin, current ) );
-    return std20::ranges::prev( std20::ranges::adjacent_find( reverse, not_pred ).base(), std::move( begin ) ); 
+    return std20::ranges::prev(
+               std20::ranges::adjacent_find( reverse.begin(), --reverse.end(), not_pred ).base(),
+               1, std::move( begin ) );
   }
 
 public:
 
   chunk_by_view() = default;
 
-  constexpr chunk_by_view( R base, Pred pred ) : 
+  constexpr chunk_by_view( R base, Pred pred ) :
       base_( std::move( base ) ), pred_( std::move( pred ) ) {}
 
   constexpr R base() const { return base_; }
@@ -204,4 +207,3 @@ NANO_INLINE_VAR(ranges::detail::chunk_by_view_fn, chunk_by)
 } // namespace njoy
 
 #endif
-
--- src/tools/std23/views/test/chunk_by.test.cpp
+++ src/tools/std23/views/test/chunk_by.test.cpp
@@ -9,6 +9,7 @@
 #include <list>
 #include <vector>
 #include "tools/std20/algorithm.hpp"
+#include <iostream>
 
 // convenience typedefs
 using namespace njoy::tools;
@@ -16,11 +17,10 @@ using namespace njoy::tools;
 SCENARIO( "chunk_by_view" ) {
 
   const std::vector< std::vector< int > > equal = { { 1, 2, 3 }, { 1, 2, 3 }, { 3 }, { 3 }, { 1, 2, 3 } };
-  const std::vector< int > modified = { -2, 0, 2, 4, 6 };
 
-  GIVEN( "a container with random access iterators" ) {
+  GIVEN( "a container with forward iterators" ) {
 
-    std::vector< int > values = { 1, 2, 3, 1, 2, 3, 3, 3, 1, 2, 3 };
+    std::forward_list< int > values = { 1, 2, 3, 1, 2, 3, 3, 3, 1, 2, 3 };
 
     WHEN( "when iterators are used" ) {
 
@@ -32,46 +32,179 @@ SCENARIO( "chunk_by_view" ) {
 
         CHECK( std20::ranges::range< Range > );
         CHECK( std20::ranges::view< Range > );
-//        CHECK( std20::ranges::sized_range< Range > );
-//        CHECK( std20::ranges::forward_range< Range > );
-//        CHECK( std20::ranges::bidirectional_range< Range > );
-//        CHECK( std20::ranges::random_access_range< Range > );
-//        CHECK( std20::ranges::contiguous_range< Range > );
-//        CHECK( std20::ranges::common_range< Range > );
+        CHECK( ! std20::ranges::sized_range< Range > );
+        CHECK( std20::ranges::forward_range< Range > );
+        CHECK( ! std20::ranges::bidirectional_range< Range > );
+        CHECK( ! std20::ranges::random_access_range< Range > );
+        CHECK( ! std20::ranges::contiguous_range< Range > );
+        CHECK( std20::ranges::common_range< Range > );
       }
 
-/*      THEN( "an chunk_by_view can be constructed and members can be tested" ) {
+      THEN( "a chunk_by_view can be constructed and members can be tested" ) {
 
-        CHECK( 5 == chunk.size() );
+        // should not compile: not sized
+        // CHECK( 5 == chunk.size() );
+
         CHECK( false == chunk.empty() );
         CHECK( true == bool( chunk ) );
 
-        CHECK( std20::ranges::equal( chunk, equal ) );
+        auto iter = chunk.begin();
+        CHECK( std20::ranges::equal( equal[0], *iter ) );
+        ++iter;
+        CHECK( std20::ranges::equal( equal[1], *iter ) );
+        ++iter;
+        CHECK( std20::ranges::equal( equal[2], *iter ) );
+        ++iter;
+        CHECK( std20::ranges::equal( equal[3], *iter ) );
+        ++iter;
+        CHECK( std20::ranges::equal( equal[4], *iter ) );
 
-        CHECK( 1 == chunk[0][0] );
-        CHECK( 2 == chunk[0][1] );
-        CHECK( 3 == chunk[0][2] );
-        CHECK( 1 == chunk[1][0] );
-        CHECK( 2 == chunk[1][1] );
-        CHECK( 3 == chunk[1][2] );
-        CHECK( 3 == chunk[2][0] );
-        CHECK( 3 == chunk[3][0] );
-        CHECK( 1 == chunk[4][0] );
-        CHECK( 2 == chunk[4][1] );
-        CHECK( 3 == chunk[4][2] );
+        CHECK( std20::ranges::equal( equal[0], chunk.front() ) );
+      } // THEN
+    } // WHEN
+  } // GIVEN
+
+  GIVEN( "a container with bidirectional iterators" ) {
+
+    std::list< int > values = { 1, 2, 3, 1, 2, 3, 3, 3, 1, 2, 3 };
+
+    WHEN( "when iterators are used" ) {
+
+      auto chunk = values | std23::views::chunk_by( std::less{} );
+      using Range = decltype(chunk);
+      using Iterator = std20::iterator_t< Range >;
+
+      THEN( "the chunk_by_view satisfies the required concepts" ) {
+
+        CHECK( std20::ranges::range< Range > );
+        CHECK( std20::ranges::view< Range > );
+        CHECK( ! std20::ranges::sized_range< Range > );
+        CHECK( std20::ranges::forward_range< Range > );
+        CHECK( std20::ranges::bidirectional_range< Range > );
+        CHECK( ! std20::ranges::random_access_range< Range > );
+        CHECK( ! std20::ranges::contiguous_range< Range > );
+        CHECK( std20::ranges::common_range< Range > );
+      }
+
+      THEN( "a chunk_by_view can be constructed and members can be tested" ) {
+
+        // should not compile: not sized
+        // CHECK( 5 == chunk.size() );
+
+        CHECK( false == chunk.empty() );
+        CHECK( true == bool( chunk ) );
+
+        auto iter = chunk.begin();
+        CHECK( std20::ranges::equal( equal[0], *iter ) );
+        ++iter;
+        CHECK( std20::ranges::equal( equal[1], *iter ) );
+        ++iter;
+        CHECK( std20::ranges::equal( equal[2], *iter ) );
+        ++iter;
+        CHECK( std20::ranges::equal( equal[3], *iter ) );
+        ++iter;
+        CHECK( std20::ranges::equal( equal[4], *iter ) );
+
+        iter = chunk.end();
+        --iter;
+        CHECK( std20::ranges::equal( equal[4], *iter ) );
+        --iter;
+        CHECK( std20::ranges::equal( equal[3], *iter ) );
+        --iter;
+        CHECK( std20::ranges::equal( equal[2], *iter ) );
+        --iter;
+        CHECK( std20::ranges::equal( equal[1], *iter ) );
+        --iter;
+        CHECK( std20::ranges::equal( equal[0], *iter ) );
+        CHECK( (*iter).begin() == values.begin() );
+        CHECK( chunk.begin() == iter );
 
         CHECK( std20::ranges::equal( equal[0], chunk.front() ) );
         CHECK( std20::ranges::equal( equal[4], chunk.back() ) );
+      } // THEN
+    } // WHEN
+  } // GIVEN
 
-        CHECK( std20::ranges::equal( chunk, modified ) );
-        CHECK( std20::ranges::equal( values, modified ) );
+  GIVEN( "a container with random access iterators" ) {
 
-        CHECK( -2 == chunk[0] );
-        CHECK(  0 == chunk[1] );
-        CHECK(  2 == chunk[2] );
-        CHECK(  4 == chunk[3] );
-        CHECK(  6 == chunk[4] );
-      } // THEN*/
+    std::vector< int > values = { 1, 2, 3, 1, 2, 3, 3, 3, 1, 2, 3 };
+
+    WHEN( "when iterators are used" ) {
+
+      auto chunk = values | std23::views::chunk_by( std::less{} );
+      using Range = decltype(chunk);
+      using Iterator = std20::iterator_t< Range >;
+      using Subrange = std20::ranges::subrange< Iterator >;
+
+      THEN( "the chunk_by_view satisfies the required concepts" ) {
+
+        CHECK( std20::ranges::range< Range > );
+        CHECK( std20::ranges::view< Range > );
+        CHECK( ! std20::ranges::sized_range< Range > );
+        CHECK( std20::ranges::forward_range< Range > );
+        CHECK( std20::ranges::bidirectional_range< Range > );
+        CHECK( ! std20::ranges::random_access_range< Range > );
+        CHECK( ! std20::ranges::contiguous_range< Range > );
+        CHECK( std20::ranges::common_range< Range > );
+      }
+
+      THEN( "a chunk_by_view can be constructed and members can be tested" ) {
+
+        // should not compile: not sized
+        // CHECK( 5 == chunk.size() );
+
+        CHECK( false == chunk.empty() );
+        CHECK( true == bool( chunk ) );
+
+        auto iter = chunk.begin();
+        CHECK( std20::ranges::equal( equal[0], *iter ) );
+        CHECK( 1 == (*iter)[0] );
+        CHECK( 2 == (*iter)[1] );
+        CHECK( 3 == (*iter)[2] );
+        ++iter;
+        CHECK( std20::ranges::equal( equal[1], *iter ) );
+        CHECK( 1 == (*iter)[0] );
+        CHECK( 2 == (*iter)[1] );
+        CHECK( 3 == (*iter)[2] );
+        ++iter;
+        CHECK( std20::ranges::equal( equal[2], *iter ) );
+        CHECK( 3 == (*iter)[0] );
+        ++iter;
+        CHECK( std20::ranges::equal( equal[3], *iter ) );
+        CHECK( 3 == (*iter)[0] );
+        ++iter;
+        CHECK( std20::ranges::equal( equal[4], *iter ) );
+        CHECK( 1 == (*iter)[0] );
+        CHECK( 2 == (*iter)[1] );
+        CHECK( 3 == (*iter)[2] );
+
+        iter = chunk.end();
+        --iter;
+        CHECK( std20::ranges::equal( equal[4], *iter ) );
+        CHECK( 1 == (*iter)[0] );
+        CHECK( 2 == (*iter)[1] );
+        CHECK( 3 == (*iter)[2] );
+        --iter;
+        CHECK( std20::ranges::equal( equal[3], *iter ) );
+        CHECK( 3 == (*iter)[0] );
+        --iter;
+        CHECK( std20::ranges::equal( equal[2], *iter ) );
+        CHECK( 3 == (*iter)[0] );
+        --iter;
+        CHECK( std20::ranges::equal( equal[1], *iter ) );
+        CHECK( 1 == (*iter)[0] );
+        CHECK( 2 == (*iter)[1] );
+        CHECK( 3 == (*iter)[2] );
+        --iter;
+        CHECK( std20::ranges::equal( equal[0], *iter ) );
+        CHECK( 1 == (*iter)[0] );
+        CHECK( 2 == (*iter)[1] );
+        CHECK( 3 == (*iter)[2] );
+        CHECK( chunk.begin() == iter );
+
+        CHECK( std20::ranges::equal( equal[0], chunk.front() ) );
+        CHECK( std20::ranges::equal( equal[4], chunk.back() ) );
+      } // THEN
     } // WHEN
   } // GIVEN
 } // SCENARIO
