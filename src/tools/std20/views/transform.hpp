a0b81a8925be08dc4eca78402b3d3c7235711533
Joe Peterson <joewpeterson@lanl.gov>
2025-03-24 17:10:16 -0600

    correct typo

--- src/tools/std20/views/transform.hpp
+++ src/tools/std20/views/transform.hpp
@@ -207,12 +207,7 @@ private:
         }
 
         friend constexpr decltype(auto) iter_move(const iterator& i)
-#ifndef __INTEL_COMPILER
-           constexpr noexcept(iter_move_noexcept_helper)
-#else
            noexcept(iter_move_noexcept_helper)
-#endif
-
         {
             if constexpr (std::is_lvalue_reference_v<decltype(*i)>) {
                 return std::move(*i);
